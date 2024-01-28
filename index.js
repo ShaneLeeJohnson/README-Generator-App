9ad1fc0da63b2e8948337a6914878d18be56ae39
shanejohnson <shane.john316@gmail.com>
2024-01-27 18:04:14 -0600

    added .gitignore file, package.json, and installed inquirer version 8.2.4. Also included the packages needed for the app and completed the writeToFile Function TODO

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            console.error('Error writing README file:', err);
+        } else {
+            console.log('README file generated successfully!')
+        }
+    });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
